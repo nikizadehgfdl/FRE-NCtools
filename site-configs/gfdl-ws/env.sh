9cdbe1217637b97d6e7add9189fd6c0ae17c8532
Niki Zadeh <Niki.Zadeh@noaa.gov>
2021-02-18 09:25:00 -0500

    Fix build environs for gfdl and gfdl-ws

--- site-configs/gfdl-ws/env.sh
+++ site-configs/gfdl-ws/env.sh
@@ -3,13 +3,16 @@
 # **********************************************************************
 source /usr/local/Modules/default/init/sh
 module use -a /home/fms/local/modulefiles
-module load netcdf/4.2
 # netcdf/4.2 loads intel-11, so unload it now
+module unload netcdf/4.2
+module unload hdf5/1.8.8
 module unload intel_compilers/11.1.073
 module load intel_compilers/18.0.5
+module load netcdf/4.2
 module load nccmp
 module use /home/sdu/publicmodules
 module load mpich2/1.5b1
+module load gcc/6.2.0
 
 # Add bats to PATH
 PATH=${PATH}:/home/sdu/opt/bats/bin
